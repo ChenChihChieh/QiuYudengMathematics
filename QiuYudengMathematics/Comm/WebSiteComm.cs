5b8eae3e133c61e6960cb60ce3703c165eb20974
Jack Chen <bj801124@outlook.com>
2021-06-08 09:03:45 +0800

    新增登入功能

---
+++ QiuYudengMathematics/Comm/WebSiteComm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace QiuYudengMathematics.Comm
+{
+    public class WebSiteComm
+    {
+        public static string CurrentUserAccount
+        {
+            get
+            {
+                FormsIdentity identity = HttpContext.Current.User.Identity as FormsIdentity;
+                if (identity == null)
+                    return string.Empty;
+                else
+                    return identity.Name;
+            }
+        }
+        public static string CurrentUserName
+        {
+            get
+            {
+                FormsIdentity identity = HttpContext.Current.User.Identity as FormsIdentity;
+                if (identity == null)
+                    return string.Empty;
+                else
+                    return identity.Ticket.UserData;
+            }
+        }
+    }
+}
---
+++ QiuYudengMathematics/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using QiuYudengMathematics.Comm;
+using QiuYudengMathematics.Models;
+using System.Configuration;
+
+namespace QiuYudengMathematics.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            FormsAuthentication.SignOut();
+            return View();
+        }
+        public ActionResult Login(LoginModel model)
+        {
+            if (model.Account == ConfigurationManager.AppSettings["adminAccount"].ToString() && model.Password == ConfigurationManager.AppSettings["adminPwd"].ToString())
+            {
+                var ticket = new FormsAuthenticationTicket(
+                version: 1,
+                name: model.Account,
+                issueDate: DateTime.Now,
+                expiration: DateTime.Now.AddDays(1),
+                isPersistent: false,
+                userData: "",
+                cookiePath: FormsAuthentication.FormsCookiePath);
+
+                var encryptedTicket = FormsAuthentication.Encrypt(ticket);
+                var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+                Response.Cookies.Add(cookie);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                TempData["Message"] = "帳號或密碼錯誤";
+                return View("Index");
+            }
+        }
+    }
+}
