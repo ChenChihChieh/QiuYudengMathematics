397624cb4c84cbd435f6c7a4c9ad82d58a9d88f5
Jack Chen <bj801124@outlook.com>
2021-06-19 10:39:17 +0800

    區分課程管理及學生課程頁

--- QiuYudengMathematics/Controllers/CourseController.cs
+++ QiuYudengMathematics/Controllers/CourseController.cs
@@ -15,15 +15,29 @@ namespace QiuYudengMathematics.Controllers
         {
             accountService = new AccountService();
         }
-        public ActionResult Index(int SubjectId)
+        #region 課程管理
+        public ActionResult CourseManagement() => View();
+
+        #endregion
+
+
+
+
+
+
+        #region 課程
+        public ActionResult CourseLearn(int SubjectId)
         {
             var Student = accountService.SingleQuery(WebSiteComm.CurrentUserAccount);
+            if (Student == null)
+                return RedirectToAction("LogoutForErrAccount", "Login", new { ErrMsg = "查無您的帳號，請通知管理人員" });
             if (!Student.Enable)
                 return RedirectToAction("LogoutForErrAccount", "Login", new { ErrMsg = "您的帳號已停用" });
-            if (!Student.Subject.Where(x => x.ID == SubjectId && x.Detriment).Any())
+            if (!Student.Subject.Where(x => x.ID == SubjectId && x.Detriment).Any() && SubjectId != -1)
                 return RedirectToAction("Index", "Home");
 
             return View();
         }
+        #endregion
     }
 }
