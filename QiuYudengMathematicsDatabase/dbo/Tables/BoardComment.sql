8a9011219c0a6de760c2006153d5688c6c8e7312
Jack Chen <bj801124@outlook.com>
2021-07-06 12:02:47 +0800

    Update EF

--- QiuYudengMathematicsDatabase/dbo/Tables/BoardComment.sql
+++ QiuYudengMathematicsDatabase/dbo/Tables/BoardComment.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[BoardComment] (
-    [Seq]              INT            NOT NULL,
+    [Seq]              INT            IDENTITY (1, 1) NOT NULL,
     [BulletinBoardSeq] INT            NOT NULL,
     [Account]          VARCHAR (10)   NOT NULL,
     [Comment]          NVARCHAR (100) NOT NULL,
     CONSTRAINT [PK_BoardLeaveComment] PRIMARY KEY CLUSTERED ([Seq] ASC),
     CONSTRAINT [FK_BoardComment_BulletinBoard] FOREIGN KEY ([BulletinBoardSeq]) REFERENCES [dbo].[BulletinBoard] ([BulletinBoardSeq])
 );
+
+
 
--- QiuYudengMathematicsDatabase/dbo/Tables/BoardSubComment.sql
+++ QiuYudengMathematicsDatabase/dbo/Tables/BoardSubComment.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[BoardSubComment] (
-    [Seq]        INT            NOT NULL,
+    [Seq]        INT            IDENTITY (1, 1) NOT NULL,
     [CommentSeq] INT            NOT NULL,
     [Account]    VARCHAR (10)   NOT NULL,
     [Comment]    NVARCHAR (100) NOT NULL,
     CONSTRAINT [PK_BoardSubLeaveComment] PRIMARY KEY CLUSTERED ([Seq] ASC),
     CONSTRAINT [FK_BoardSubComment_BoardComment] FOREIGN KEY ([CommentSeq]) REFERENCES [dbo].[BoardComment] ([Seq])
 );
+
+
 
