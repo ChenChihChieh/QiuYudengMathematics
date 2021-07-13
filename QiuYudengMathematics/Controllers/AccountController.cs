df1a1bf6ecf59fbb1881eadc735e914e563f5f3e
Jack Chen <bj801124@outlook.com>
2021-07-13 13:58:22 +0800

    移除未用的Model

--- QiuYudengMathematics/Controllers/AccountController.cs
+++ QiuYudengMathematics/Controllers/AccountController.cs
@@ -16,7 +16,7 @@ namespace QiuYudengMathematics.Controllers
             SubjectService = new SubjectService();
             DeviceService = new DeviceService();
         }
-        public ActionResult Index() => View(AccountService.getGrade());
+        public ActionResult Index() => View();
         public ActionResult Query(AccountQueryModel model) => Json(new RtnModel() { Success = true, Data = AccountService.Query(model) }, JsonRequestBehavior.AllowGet);
         public ActionResult SingleQuery(string Id) => Json(new RtnModel() { Success = true, Data = AccountService.SingleQuery(Id) }, JsonRequestBehavior.AllowGet);
         public ActionResult GetGradeSubject() => Json(new RtnModel() { Success = true, Data = SubjectService.getGradeSubject() }, JsonRequestBehavior.AllowGet);
